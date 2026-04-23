670afe7db78b2bd931b6be7ce9a8a03fb3cb439d
vlaktron <tkapuranis@vlaktron.com>
2026-04-22 23:25:50 -0600

    sub-muse-psi.8: add url.Values support
    - Add buildRequestWithValues method
    - Add sendRequestWithValues method
    - Support multi-value parameters via url.Values

--- internal/subsonic/client.go
+++ internal/subsonic/client.go
@@ -151,3 +151,98 @@ func (c *Client) sendRequest(endpoint string, params map[string]string, result i
 
 	return json.Unmarshal(outer.Inner, result)
 }
+
+func (c *Client) buildRequestWithValues(endpoint string, params url.Values) (string, error) {
+	u, err := url.Parse(c.baseURL)
+	if err != nil {
+		return "", err
+	}
+
+	u.Path = "/rest/" + endpoint
+
+	query := u.Query()
+	query.Set("u", c.username)
+
+	switch c.authMode {
+	case AuthToken:
+		salt, err := generateSalt()
+		if err != nil {
+			return "", err
+		}
+		hash := md5.Sum([]byte(c.password + salt))
+		query.Set("t", hex.EncodeToString(hash[:]))
+		query.Set("s", salt)
+	default:
+		query.Set("p", c.password)
+	}
+
+	query.Set("v", "1.16.1")
+	query.Set("c", c.clientName)
+	query.Set("f", "json")
+
+	for key, values := range params {
+		for _, v := range values {
+			query.Add(key, v)
+		}
+	}
+
+	u.RawQuery = query.Encode()
+	return u.String(), nil
+}
+
+func (c *Client) sendRequestWithValues(endpoint string, params url.Values, result interface{}) error {
+	requestURL, err := c.buildRequestWithValues(endpoint, params)
+	if err != nil {
+		return err
+	}
+
+	req, err := http.NewRequest("GET", requestURL, nil)
+	if err != nil {
+		return err
+	}
+
+	resp, err := c.httpClient.Do(req)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Errorf("HTTP %d: %s", resp.StatusCode, resp.Status)
+	}
+
+	bodyBytes, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return err
+	}
+
+	// Unwrap the outer "subsonic-response" envelope
+	var outer struct {
+		Inner json.RawMessage `json:"subsonic-response"`
+	}
+	if err := json.Unmarshal(bodyBytes, &outer); err != nil {
+		return err
+	}
+
+	// Check status
+	var statusCheck struct {
+		Status string `json:"status"`
+		Error  *Error `json:"error"`
+	}
+	if err := json.Unmarshal(outer.Inner, &statusCheck); err != nil {
+		return err
+	}
+
+	if statusCheck.Status != "ok" {
+		if statusCheck.Error != nil {
+			return fmt.Errorf("API error %d: %s", statusCheck.Error.Code, statusCheck.Error.Message)
+		}
+		return fmt.Errorf("API error: status=%s", statusCheck.Status)
+	}
+
+	if result == nil {
+		return nil
+	}
+
+	return json.Unmarshal(outer.Inner, result)
+}
