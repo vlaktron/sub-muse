0bdd8dc1f716920ab34043a98f4343de10864ee5
vlaktron <tkapuranis@vlaktron.com>
2026-04-23 23:03:40 -0600

    feat(ui): implement tab switching and navigation

--- internal/ui/commands.go
+++ internal/ui/commands.go
@@ -33,3 +33,23 @@ func loadPlaylistsCmd(client *subsonic.Client) tea.Cmd {
 		return playlistsLoadedMsg{playlists: playlists, err: err}
 	}
 }
+
+func loadAlbumDetailCmd(client *subsonic.Client, albumID string) tea.Cmd {
+	return func() tea.Msg {
+		album, err := client.GetAlbum(albumID)
+		return albumDetailMsg{album: album, err: err}
+	}
+}
+
+func loadArtistDetailCmd(client *subsonic.Client, artistID string) tea.Cmd {
+	return func() tea.Msg {
+		artist, err := client.GetArtist(artistID)
+		return artistDetailMsg{artist: artist, err: err}
+	}
+}
+
+func playSongCmd(client *subsonic.Client, song subsonic.Song) tea.Cmd {
+	return func() tea.Msg {
+		return playbackStartedMsg{song: song}
+	}
+}
